8a54a2116653ce50a24e4bed04545a17774204a7
natek <natekinderman16@gmail.com>
2019-09-04 09:38:15 -0500

    Created repo and customer class and employee class

---
+++ TrashCollector1/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector1.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
